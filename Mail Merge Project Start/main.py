f83747924fa91f395dc4cdc242d1c9db6f21a8a4
Emir Karayağız <emirkyz61@hotmail.com>
2022-09-10 19:17:57 +0300

    mail auto change placeholder texts

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,8 +1,15 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+PLACEHOLDERR = "[name]"
+
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
     
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDERR,stripped_name)      
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}","w") as complete_letter:
+            complete_letter.write(new_letter)
+   
