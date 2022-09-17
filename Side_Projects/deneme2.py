ba11cfe69309e7a2404e2f7ef0d05ebb3e0bb62e
Emir Karayağız <emirkyz61@hotmail.com>
2022-09-17 16:44:20 +0300

    dsada

--- Side_Projects/deneme2.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-
-data = np.random.rand(4,3)
-dataframe = pd.DataFrame(data)
-print(dataframe)
--- Side_Projects/import imp.py
+++
@@ -1,9 +0,0 @@
-import imp
-
-
-import pandas as pd
-import numpy as np
-
-a = np.random.rand(4,3)
-pd.DataFrame(a)
-print(a)
